8c0bf11c84f62bae317cf378bca35bca6e0e3280
Gemar Usi <74299026+Gemar30@users.noreply.github.com>
2021-10-20 22:41:34 +0800

    update python t

--- Day1.py
+++ Day1.py
@@ -8,6 +8,7 @@
 
 # main.py
 
+# PRINTING
 
 #print("Day 1 - Python Print Function")
 #print("The function is declared like this: ")
@@ -18,8 +19,41 @@
 #print("Hello" + " " + "Mich")
 
 
-#print("Day 1 - String Manipulation")
-#print('String concantenation is done with the "+" sign. ')
-#print('e.g print("Hello" + "world")')
-#print("New lines can be created with a backslash and n.")
+# print("Day 1 - String Manipulation")
+# print('String concantenation is done with the "+" sign. ')
+# print('e.g print("Hello" + "world")')
+# print("New lines can be created with a backslash and n.")
+
+# INPUTTING
+
+# print("Hello " + input("What is your name? ") + "!")
+
+# INPUTTING EXERCISES
+
+# name = input("What is your name? ")
+# print(len(name))
+
+# print(len(input("What is your name? ")))
+
+# PYTHON VARIABLES
+
+# name = input("What is your name? ")
+# length = len(name)
+
+# print(length)
+
+# Variable Exercises
+
+a = input("a: ")
+b = input("b: ")
+
+# a = 100
+# b = 50
+
+# c = a #100
+# a = b #50
+# b = c #100
+
+# print("a= " + a)
+# print("b= " + b)
 
