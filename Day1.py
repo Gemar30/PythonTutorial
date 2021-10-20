d800e4da59a2bfe3cbb6b74992fc563144f09c53
Gemar Usi <74299026+Gemar30@users.noreply.github.com>
2021-10-20 20:21:56 +0800

    Create Day1.py

---
+++ Day1.py
@@ -0,0 +1,25 @@
+# Printing
+# Documenting
+# Commenting
+# String Manipulation
+# Variables
+
+# Band Name Generator
+
+# main.py
+
+
+#print("Day 1 - Python Print Function")
+#print("The function is declared like this: ")
+#print("print ('What to print')")
+
+# print("Hello World! \nHello World \nHello World" )
+
+#print("Hello" + " " + "Mich")
+
+
+#print("Day 1 - String Manipulation")
+#print('String concantenation is done with the "+" sign. ')
+#print('e.g print("Hello" + "world")')
+#print("New lines can be created with a backslash and n.")
+
