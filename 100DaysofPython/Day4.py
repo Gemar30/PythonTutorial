73fc3d4ac56d9832e28fa7bad4a658318d52fd90
Gemar Usi <74299026+Gemar30@users.noreply.github.com>
2021-10-24 18:54:54 +0800

    updated day 4

---
+++ 100DaysofPython/Day4.py
@@ -0,0 +1,43 @@
+# Day 4 
+
+# Randomization and Python List
+# Module - split the code into individual module where each module responsible for different functionality.
+import random
+
+
+# random_integer = random.randint(1, 10)
+# print(random_integer)
+
+
+# random_float = random.random() * 5
+# print(random_float)
+
+# love_score = random.randint(1, 100)
+# print(f"Your love score is {love_score}")
+
+
+
+#List - Data structure - Organizing and storing data in python.
+
+# states_of_america = ["Delaware", "Pennsylvania"]
+
+# print(states_of_america[2])
+
+# Banker Roulette - Who will pay the bill?
+
+# test_speed = int(input("Create a seed number: "))
+# random.seed(test_speed)
+
+# nameAsCSV = input("Give me everybody's names, seperated by a comma. ")
+# names = nameAsCSV.split(", ")
+
+# # Get the total number of items in the list.
+# num_items = len(names)
+
+# # Generate random numbers  between 0 and last index
+# # random_choice =random.randint(0, num_items - 1)
+
+# # person_who_will_pay = names[random_choice]
+
+# # print(person_who_will_pay + " is going to buy the meal")
+
