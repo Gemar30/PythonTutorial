a16ea0a2fc460574d4e6e17a389c5eacbb725a99
Gemar Usi <74299026+Gemar30@users.noreply.github.com>
2021-10-26 16:17:21 +0800

    Day 6 updated

--- 100DaysofPython/Day5.py
+++ 100DaysofPython/Day5.py
@@ -13,7 +13,7 @@
 # AVERAGE HEIGHT EXERCISE
 
 # student_heights = input("Input a list of student heights ").split()
-
+    
 # for n in range(0, len(student_heights)):
 #     student_heights[n] = int(student_heights[n])
 # #print(student_heights)
---
+++ 100DaysofPython/Day6.py
@@ -0,0 +1,124 @@
+# Day 6
+
+# Function, Code blocks, While loops
+
+# Function
+# def my_function():
+#     print("Hello")
+#     print("bye")
+
+# my_function()
+
+# REEBORGS WORLD
+# Hurdle 1
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+
+# def jump():
+#     move()
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
+
+# for step in range(6):
+#     jump()
+
+
+# WHILE LOOPS
+# number_of_hurdles = 6
+
+# while number_of_hurdles > 0:
+#     jump()
+#     number_of_hurdles -=1
+#     print(number_of_hurdles)
+
+# Hurdle 2
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+
+# def jump():
+#     move()
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
+
+# while at_goal != True: or while not at_goal()
+#     jump()
+    
+# Hurdle 3
+
+# def turn_right():
+#      turn_left()
+#      turn_left()
+#      turn_left()
+
+# def jump():
+#      turn_left()
+#      move()
+#      turn_right()
+#      move()
+#      turn_right()
+#      move()
+#      turn_left()
+
+# while not at_goal():
+#     if wall_in_front():
+#         jump()
+#     else:
+#         move()
+
+# Hurdle 4
+
+# def turn_right():
+#      turn_left()
+#      turn_left()
+#      turn_left()
+
+# def jump():
+#      turn_left()
+#      while wall_on_right():
+#         move()
+#      turn_right()
+#      move()
+#      turn_right()
+  
+#      while front_is_clear():
+#         move()
+#      turn_left()
+    
+# while not at_goal():
+#     if wall_in_front():
+#         jump()
+#     else:
+#         move()
+    
+# MAZE
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+ 
+# while not at_goal():
+#     if right_is_clear():
+#         turn_right()
+#         move()
+#     elif front_is_clear():
+#         move()
+#     else:
+#         turn_left()
+        
